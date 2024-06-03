8aad62619ed5eb67554ea1b5d71047a96800205e
Mudite55 <93543779+Mudite55@users.noreply.github.com>
2024-06-03 17:40:00 +0300

    Add files via upload

---
+++ mainigais piemers.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = 20;
+
+let sum = x + y;
+let product = x * y;
+
+console.log("Summa: " + sum); // Izvadīs: Summa: 30
+console.log("Reizinājums: " + product); // Izvadīs: Reizinājums: 200
+
+function addNumbers(x, y) {
+    return x + y;
+  }
+  
+  let sum = addNumbers(10, 20);
+  console.log("Summa: " + sum); // Izvadīs: Summa: 30
