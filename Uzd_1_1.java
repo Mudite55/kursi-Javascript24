53b53faf7d2526d6ebbdeb5de82a541cced29f6d
Mudite55 <93543779+Mudite55@users.noreply.github.com>
2024-10-28 18:08:41 +0200

    Add files via upload

---
+++ Uzd_1_1.java
@@ -0,0 +1,23 @@
+
+ public class Uzd_1_1 {
+
+public static void main(String[] args) {
+
+		System.out.println("Mudîte");
+		System.out.println("Ramute");
+		System.out.println("|\\_________/|");
+		System.out.println("|           |");
+		System.out.println("|[o]     [o]|");
+		System.out.println("|     v     |");
+		System.out.println("|           |");
+		System.out.println("|           |");
+		System.out.println("|           |");
+		System.out.println("-ooo---ooo- |");
+		
+
+}
+ }
+	
+	
+	
+ 
